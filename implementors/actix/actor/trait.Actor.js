68a88b07a4c138c5125b843a60a4ab43789d2499
Travis CI User <travis@example.org>
2018-02-04 19:11:57 +0000

    Documentation upload

---
+++ implementors/actix/actor/trait.Actor.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl Actor for <a class=\"struct\" href=\"actix_web/client/struct.ClientConnector.html\" title=\"struct actix_web::client::ClientConnector\">ClientConnector</a>","impl&lt;T, A, H, U, V&gt; Actor for <a class=\"struct\" href=\"actix_web/struct.HttpServer.html\" title=\"struct actix_web::HttpServer\">HttpServer</a>&lt;T, A, H, U&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: 'static,<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"actix_web/server/trait.IoStream.html\" title=\"trait actix_web::server::IoStream\">IoStream</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;H: <a class=\"trait\" href=\"actix_web/server/trait.HttpHandler.html\" title=\"trait actix_web::server::HttpHandler\">HttpHandler</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;U: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a>&lt;Item = V&gt; + 'static,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: <a class=\"trait\" href=\"actix_web/server/trait.IntoHttpHandler.html\" title=\"trait actix_web::server::IntoHttpHandler\">IntoHttpHandler</a>&lt;Handler = H&gt;,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"actix_web/error/struct.InternalError.html\" title=\"struct actix_web::error::InternalError\">InternalError</a>&lt;T&gt;","impl&lt;T, A, H, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"actix_web/struct.HttpServer.html\" title=\"struct actix_web::HttpServer\">HttpServer</a>&lt;T, A, H, U&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;H: <a class=\"trait\" href=\"actix_web/server/trait.HttpHandler.html\" title=\"trait actix_web::server::HttpHandler\">HttpHandler</a> + 'static,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
